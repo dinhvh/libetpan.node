a851734c5eaaf26ed94ebe55e40f6b4217703fb8
Yazhong Liu <l900422@vip.qq.com>
2014-05-03 15:59:13 +0800

    move api to new version and add tests

---
+++ tests/test-greeting.js
@@ -0,0 +1,36 @@
+
+var test = require('tape');
+var etpan = require('../lib/libetpan').etpan;
+var Capabilities = require('../lib/libetpan').option.Capabilities;
+
+test('empty response should throw error', function(t) {
+  var emptyRes = '';
+  var r = etpan.responseParse(emptyRes, etpan.PARSER_ENABLE_GREETING);
+  t.equal(r.result, etpan.MAILIMAP_ERROR_PARSE);
+  t.end();
+});
+
+test('greeting response without crlf', function(t) {
+  var greetingRes = '* OK [CAPABILITY IMAP4 IMAP4rev1 IDLE XAPPLEPUSHSERVICE ID UIDPLUS AUTH=LOGIN NAMESPACE] QQMail IMAP4Server ready';
+  var r = etpan.responseParse(greetingRes, etpan.PARSER_ENABLE_GREETING);
+  // This should be returns MAILIMAP_ERROR_NEEDS_MORE_DATA(42)?
+  t.equal(r.result, etpan.MAILIMAP_ERROR_PARSE);
+  t.end();
+});
+
+test('greeting response', function(t) {
+  var matches = [
+    'Idle',
+    'Id',
+    'UIDPlus',
+    'AuthLogin',
+    'Namespace'
+  ];
+  var greetingRes = '* OK [CAPABILITY IMAP4 IMAP4rev1 IDLE XAPPLEPUSHSERVICE ID UIDPLUS AUTH=LOGIN NAMESPACE] QQMail IMAP4Server ready\r\n';
+  var r = etpan.responseParse(greetingRes, etpan.PARSER_ENABLE_GREETING);
+  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  r.getCapabilitiesFromResponse().forEach(function(item, index) {
+    t.equal(Capabilities[item], matches[index]);
+  });
+  t.end();
+});
---
+++ tests/test-response.js
@@ -0,0 +1,40 @@
+
+var test = require('tape');
+var etpan = require('../lib/libetpan').etpan;
+
+test('LIST response', function(t) {
+  var res = '* LIST (\\Noselect) "/" ~/Mail/foo\r\nA001 OK LIST\r\n';
+  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  t.end();
+});
+
+test('LSUB response', function(t) {
+  var res = '* LSUB () "." #news.comp.mail.misc\r\nA001 OK LSUB\r\n';
+  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  t.end();
+});
+
+test('STATUS response', function(t) {
+  var res = '* FLAGS (\\Answered \\Flagged \\Deleted \\Seen \\Draft)\r\nA001 OK FLAGS\r\n';
+  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  t.end();
+});
+
+test('SEARCH response', function(t) {
+  var res = '* SEARCH 2 3 6\r\nA001 OK SEARCH\r\n';
+  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  t.end();
+});
+
+test('FETCH response', function(t) {
+  var res = '* 23 FETCH (FLAGS (\\Seen) RFC822.SIZE 44827)\r\nA001 OK FETCH\r\n';
+  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
+  var items = r.getFetchItemsFromResponse();
+  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  t.equal(items[0].size, 44827);
+  t.end();
+});
