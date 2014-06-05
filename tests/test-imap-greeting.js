e284851d467a6a96193426905455eb6085f79611
Yazhong Liu <yorkiefixer@gmail.com>
2014-06-05 14:26:18 +0800

    move all constants to constants.json

--- tests/test-imap-greeting.js
+++ tests/test-imap-greeting.js
@@ -1,20 +1,21 @@
 
 var test = require('tape');
 var etpan = require('../lib/libetpan').etpan;
-var Capabilities = require('../lib/libetpan').option.Capabilities;
+var Constants = require('../lib/libetpan').Constants;
+var Capabilities = Constants.Capabilities;
 
 test('empty response should throw error', function(t) {
   var emptyRes = '';
-  var r = etpan.responseParse(emptyRes, etpan.PARSER_ENABLE_GREETING);
-  t.equal(r.result, etpan.MAILIMAP_ERROR_PARSE);
+  var r = etpan.responseParse(emptyRes, Constants.ParsingOption.PARSER_ENABLE_GREETING);
+  t.equal(r.result, Constants.Errno.MAILIMAP_ERROR_PARSE);
   t.end();
 });
 
 test('greeting response without crlf', function(t) {
   var greetingRes = '* OK [CAPABILITY IMAP4 IMAP4rev1 IDLE XAPPLEPUSHSERVICE ID UIDPLUS AUTH=LOGIN NAMESPACE] QQMail IMAP4Server ready';
-  var r = etpan.responseParse(greetingRes, etpan.PARSER_ENABLE_GREETING);
+  var r = etpan.responseParse(greetingRes, Constants.ParsingOption.PARSER_ENABLE_GREETING);
   // This should be returns MAILIMAP_ERROR_NEEDS_MORE_DATA(42)?
-  t.equal(r.result, etpan.MAILIMAP_ERROR_PARSE);
+  t.equal(r.result, Constants.Errno.MAILIMAP_ERROR_PARSE);
   t.end();
 });
 
@@ -27,7 +28,7 @@ test('greeting response', function(t) {
     'Namespace'
   ];
   var greetingRes = '* OK [CAPABILITY IMAP4 IMAP4rev1 IDLE XAPPLEPUSHSERVICE ID UIDPLUS AUTH=LOGIN NAMESPACE] QQMail IMAP4Server ready\r\n';
-  var r = etpan.responseParse(greetingRes, etpan.PARSER_ENABLE_GREETING);
+  var r = etpan.responseParse(greetingRes, Constants.ParsingOption.PARSER_ENABLE_GREETING);
   t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
   r.getCapabilitiesFromResponse().forEach(function(item, index) {
     t.equal(Capabilities[item], matches[index]);
--- tests/test-response.js
+++ tests/test-response.js
@@ -1,40 +1,41 @@
 
 var test = require('tape');
 var etpan = require('../lib/libetpan').etpan;
+var Constants = require('../lib/libetpan').Constants;
 
 test('LIST response', function(t) {
   var res = '* LIST (\\Noselect) "/" ~/Mail/foo\r\nA001 OK LIST\r\n';
-  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
-  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  var r = etpan.responseParse(res, Constants.ParsingOption.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, Constants.Errno.MAILIMAP_NO_ERROR);
   t.end();
 });
 
 test('LSUB response', function(t) {
   var res = '* LSUB () "." #news.comp.mail.misc\r\nA001 OK LSUB\r\n';
-  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
-  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  var r = etpan.responseParse(res, Constants.ParsingOption.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, Constants.Errno.MAILIMAP_NO_ERROR);
   t.end();
 });
 
 test('STATUS response', function(t) {
   var res = '* FLAGS (\\Answered \\Flagged \\Deleted \\Seen \\Draft)\r\nA001 OK FLAGS\r\n';
-  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
-  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  var r = etpan.responseParse(res, Constants.ParsingOption.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, Constants.Errno.MAILIMAP_NO_ERROR);
   t.end();
 });
 
 test('SEARCH response', function(t) {
   var res = '* SEARCH 2 3 6\r\nA001 OK SEARCH\r\n';
-  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
-  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  var r = etpan.responseParse(res, Constants.ParsingOption.PARSER_ENABLE_RESPONSE);
+  t.equal(r.result, Constants.Errno.MAILIMAP_NO_ERROR);
   t.end();
 });
 
 test('FETCH response', function(t) {
   var res = '* 23 FETCH (FLAGS (\\Seen) RFC822.SIZE 44827)\r\nA001 OK FETCH\r\n';
-  var r = etpan.responseParse(res, etpan.PARSER_ENABLE_RESPONSE);
+  var r = etpan.responseParse(res, Constants.ParsingOption.PARSER_ENABLE_RESPONSE);
   var items = r.getFetchItemsFromResponse();
-  t.equal(r.result, etpan.MAILIMAP_NO_ERROR);
+  t.equal(r.result, Constants.Errno.MAILIMAP_NO_ERROR);
   t.equal(items[0].size, 44827);
   t.end();
 });
